fc08e8e3258a886dee8d1511d7272d70c502d122
pijarosa <pijarosa@student.wszib.edu>
2022-06-05 16:58:55 +0200

    Finalizacja aplikacji- dodany komentarz

--- src/test/java/pl/edu/wszib/jwdcollors/service/impl/SelectedColorServiceImplTest.java
+++ src/test/java/pl/edu/wszib/jwdcollors/service/impl/SelectedColorServiceImplTest.java
@@ -1,18 +1,24 @@
 package pl.edu.wszib.jwdcollors.service.impl;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SelectedColorServiceImplTest {
 
+    // testy dla dao, dla serwisów, i jednostkowe
+
+    @BeforeEach
+    void setUp() {
+    }
+
+
     @Test
     void save() {
     }
 
-    @Test
-    void testSave() {
-    }
+
 
     @Test
     void getAllData() {
