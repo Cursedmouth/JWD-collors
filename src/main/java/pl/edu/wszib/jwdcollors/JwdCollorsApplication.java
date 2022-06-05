cda7e40ecea7feda422e45d7db398699aef83bf9
pijarosa <pijarosa@student.wszib.edu>
2022-06-05 09:26:20 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/jwdcollors/JwdCollorsApplication.java
@@ -0,0 +1,14 @@
+package pl.edu.wszib.jwdcollors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwdCollorsApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(JwdCollorsApplication.class, args);
+    }
+
+}
